430e15e0258976f63cea6d6dff3b3ce9e5e46137
Simon Pilkington <simonpi@amazon.com>
2020-02-13 14:45:02 -0800

    Don't use opaque return types.

--- Sources/SmokeOperationsHTTP1/SmokeHTTP1Server+startAsOperationServer.swift
+++ Sources/SmokeOperationsHTTP1/SmokeHTTP1Server+startAsOperationServer.swift
@@ -22,61 +22,6 @@ import Logging
 import SmokeInvocation
 
 public extension SmokeHTTP1Server {
-    
-    /**
-     Creates and starts a SmokeHTTP1Server to handle operations using the
-     provided handlerSelector. This call will return once the server has started.
-     
-     On Linux with Swift 5.1 the function returns an opaque return type.
-     
-     - Parameters:
-         - handlerSelector: the selector that will provide an operation
-                            handler for a operation request
-         - context: the context to pass to operation handlers.
-         - port: Optionally the localhost port for the server to listen on.
-                 If not specified, defaults to 8080.
-         - invocationStrategy: Optionally the invocation strategy for incoming requests.
-                               If not specified, the handler for incoming requests will
-                               be invoked on DispatchQueue.global().
-         - eventLoopProvider: Provides the event loop to be used by the server.
-                              If not specified, the server will create a new multi-threaded event loop
-                              with the number of threads specified by `System.coreCount`.
-         - shutdownOnSignal: Specifies if the server should be shutdown when a signal is received.
-                             If not specified, the server will be shutdown if a SIGINT is received.
-     - Returns: the SmokeHTTP1Server that was created and started.
-     */
-    #if os(Linux) && swift(>=5.1)
-    static func startAsOperationServer<ContextType, SelectorType, OperationIdentifer>(
-        withHandlerSelector handlerSelector: SelectorType,
-        andContext context: ContextType,
-        andPort port: Int = ServerDefaults.defaultPort,
-        serverName: String = "Server",
-        invocationStrategy: InvocationStrategy = GlobalDispatchQueueAsyncInvocationStrategy(),
-        defaultLogger: Logger = Logger(label: "com.amazon.SmokeFramework.SmokeHTTP1Server"),
-        reportingConfiguration: SmokeServerReportingConfiguration<OperationIdentifer> = SmokeServerReportingConfiguration(),
-        eventLoopProvider: SmokeServerEventLoopProvider = .spawnNewThreads,
-        shutdownOnSignal: SmokeServerShutdownOnSignal = .sigint) throws -> some SmokeHTTP1Server
-        where SelectorType: SmokeHTTP1HandlerSelector, SelectorType.ContextType == ContextType,
-        HTTPRequestHead == SelectorType.DefaultOperationDelegateType.TraceContextType.RequestHeadType,
-        SelectorType.DefaultOperationDelegateType.RequestHeadType == SmokeHTTP1RequestHead,
-        SelectorType.DefaultOperationDelegateType.ResponseHandlerType ==
-            StandardHTTP1ResponseHandler<SmokeServerInvocationContext<SelectorType.DefaultOperationDelegateType.TraceContextType>>,
-        SelectorType.OperationIdentifer == OperationIdentifer {
-            let handler = OperationServerHTTP1RequestHandler(
-                handlerSelector: handlerSelector,
-                context: context, serverName: serverName, reportingConfiguration: reportingConfiguration)
-            let server = StandardSmokeHTTP1Server(handler: handler,
-                                                  port: port,
-                                                  invocationStrategy: invocationStrategy,
-                                                  defaultLogger: defaultLogger,
-                                                  eventLoopProvider: eventLoopProvider,
-                                                  shutdownOnSignal: shutdownOnSignal)
-            
-            try server.start()
-            
-            return server
-    }
-    #else
     static func startAsOperationServer<ContextType, SelectorType, OperationIdentifer>(
         withHandlerSelector handlerSelector: SelectorType,
         andContext context: ContextType,
@@ -107,5 +52,4 @@ public extension SmokeHTTP1Server {
             
             return server
     }
-    #endif
 }
