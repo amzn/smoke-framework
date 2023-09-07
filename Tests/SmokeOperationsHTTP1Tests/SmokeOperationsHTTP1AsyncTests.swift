2137785f3dedd73df383e6478f4065c58f40e11e
Simon Pilkington <simonpi@amazon.com>
2023-09-07 10:40:00 -0400

    Rely on test discovery.

--- Tests/SmokeOperationsHTTP1Tests/SmokeOperationsHTTP1AsyncTests.swift
+++ Tests/SmokeOperationsHTTP1Tests/SmokeOperationsHTTP1AsyncTests.swift
@@ -379,21 +379,4 @@ class SmokeOperationsHTTP1AsyncTests: XCTestCase {
         
         XCTAssertEqual("InvalidOperation", output.type)
     }
-
-    static var allTests = [
-        ("testExampleHandler", testExampleHandler),
-        ("testExampleHandlerWithTokenHeaderQuery", testExampleHandlerWithTokenHeaderQuery),
-        ("testExampleVoidHandler", testExampleVoidHandler),
-        ("testExampleVoidHandlerWithTokenHeaderQuery", testExampleVoidHandlerWithTokenHeaderQuery),
-        ("testInputValidationError", testInputValidationError),
-        ("testInputValidationErrorWithTokenHeaderQuery", testInputValidationErrorWithTokenHeaderQuery),
-        ("testOutputValidationError", testOutputValidationError),
-        ("testOutputValidationErrorWithTokenHeaderQuery", testOutputValidationErrorWithTokenHeaderQuery),
-        ("testThrownError", testThrownError),
-        ("testThrownErrorWithTokenHeaderQuery", testThrownErrorWithTokenHeaderQuery),
-        ("testInvalidOperation", testInvalidOperation),
-        ("testInvalidOperationWithTokenHeaderQuery", testInvalidOperationWithTokenHeaderQuery),
-        ("testIncorrectHTTPMethodOperation", testIncorrectHTTPMethodOperation),
-        ("testIncorrectHTTPMethodOperationWithTokenHeaderQuery", testIncorrectHTTPMethodOperationWithTokenHeaderQuery),
-    ]
 }
